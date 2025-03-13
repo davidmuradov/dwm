17436a448e24bafd5bd8fdc91dfcfc1be2b5ebcd
davidmuradov <david.linuxterminal@gmail.com>
2025-03-12 23:45:24 -0400

    colors update

--- config.h
+++ config.h
@@ -9,16 +9,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono:size=14" };
 static const char dmenufont[]       = "JetBrains Mono:size=14";
-static const char col_gray1[]       = "#2E3440";
-static const char col_gray2[]       = "#1a1a1a";
-static const char col_gray3[]       = "#FFFFFF";
-static const char col_gray4[]       = "#000000";
-static const char col_cyan[]        = "#919191";
-static const char col_blue[]        = "#5e81ac";
+static const char col_gray1[]       = "#2E3440"; // Dark gray
+static const char col_gray2[]       = "#3b4252"; // nord1
+static const char col_white[]       = "#eceff4"; // white
+static const char col_fg_selected[] = "#2E3440";
+static const char col_cyan[]        = "#88c0d0"; // nordlightblue
+static const char col_blue[]        = "#5e81ac"; // nordblue
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_blue  },
+	[SchemeNorm] = { col_white, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_fg_selected, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_cyan, "-sf", col_fg_selected, "-c", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *printscreencmd[] = { "scrot", "-s", "/tmp/screenshot.png", "-f", NULL };
 static const char *browsercmd[] = { "librewolf", NULL };
